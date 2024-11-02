60ea241c4cb9a0025eb4ca412ab27e7bea1b5923
Areeba Irfan <161799302+Areeba-irfan-Github-account@users.noreply.github.com>
2024-11-02 16:06:55 +0500

    edite-

---
+++ .next/static/chunks/app_login_page_tsx_d91b07._.js
@@ -0,0 +1,11 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/app_login_page_tsx_d91b07._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/app_login_page_tsx_d91b07._.js",
+  "chunks": [
+    "static/chunks/[root of the server]__3a708d._.js"
+  ],
+  "source": "dynamic"
+});
